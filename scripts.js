9e13e3df0b4c4626d40630de581cdbb2162a0e58
Patryk-Wawa <nadawcamaila@o2.pl>
2017-07-30 13:40:20 +0200

    js comment

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+//pierwszy komentarz, liniowy
+
+/*drugi komentarz, blokowy,
+kilka linii na raz*/
